31471fc26ddc20398378a3ef9548299e3b72e13f
Einar Brizzo <einar_brizzo@protonmail.com>
2020-10-17 01:53:02 +0300

    created url and response

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,14 @@
-token = "1393764739:AAHdgG7ehkUtXVkN14lzI9E73RRHTuxUrIU"
+import requests
+
+TOKEN = "1393764739:AAHdgG7ehkUtXVkN14lzI9E73RRHTuxUrIU"
+
+URL = "https://api.telegram.org/bot{token}/{method}"
+
+updates = "getUpdates"  # getUpdates - метод для получения обновлений в telegram API
+
+url = URL.format(token=TOKEN, method=updates)
+
+# print(url)
+
+response = requests.get(url)  # sending get-request
+print(response)
